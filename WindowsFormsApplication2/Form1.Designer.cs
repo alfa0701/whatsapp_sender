143a5df9e41774baea6b545ae1e0ee90ba64b1cf
tnt2017 <tnt2011@bk.ru>
2019-02-03 13:13:15 +0700

    Add files via upload

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button12 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(29, 143);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Набрать текст";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(29, 26);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(147, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Запуск whatsap\r\np";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(29, 55);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(147, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "К списку контактов";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(29, 114);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(147, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Кликнуть контакт";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(29, 172);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(147, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Отправить сообщение";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(29, 201);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(147, 23);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Возврат к списку контактов";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(29, 248);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(147, 23);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "Отправить одному";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(29, 277);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(147, 23);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "Отправить всем";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(29, 84);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(147, 23);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "Промотать";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(193, 84);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDown1.TabIndex = 11;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 329);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(251, 82);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.Text = "123456";
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(182, 143);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(136, 23);
+            this.button12.TabIndex = 15;
+            this.button12.Text = "Текст через буфер";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 423);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Name = "Form1";
+            this.Text = "WHATSAPP SENDER v0.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button12;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices; // эту строку добавил стало меньше ошибок
+using System.Diagnostics;
+using System.Threading;
+using System.Globalization;
+using System.IO;
+
+
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, IntPtr lParam);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int PostMessage(IntPtr hwnd, int wMsg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("USER32.DLL", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindow(string lpClassName,
+            string lpWindowName);
+
+        // Activate an application window.
+        [DllImport("USER32.DLL")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+        
+        [DllImport("USER32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(MouseEvent dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+        
+        [DllImport("user32.dll")]
+        public static extern int ActivateKeyboardLayout(int HKL, int flags);
+        
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetKeyboardLayout(uint thid);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern IntPtr ActivateKeyboardLayout(IntPtr hkl, uint Flags);
+        
+        const int BM_SETSTATE = 243;
+        const int WM_LBUTTONDOWN = 513;
+        const int WM_LBUTTONUP = 514;
+        const int WM_KEYDOWN = 256;
+        const int WM_CHAR = 258;
+        const int WM_KEYUP = 257;
+        const int WM_SETFOCUS = 7;
+        const int WM_SYSCOMMAND = 274;
+        const int SC_MINIMIZE = 32;
+
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetWindowPos(IntPtr hWnd,
+            IntPtr hWndInsertAfter, int X, int Y, int cx, int cy,
+            SetWindowPosFlags uFlags);
+
+        // Определим перечисление SetWindowPosFlags.
+        [Flags()]
+        private enum SetWindowPosFlags : uint
+        {
+            SynchronousWindowPosition = 0x4000,
+            DeferErase = 0x2000,
+            DrawFrame = 0x0020,
+            FrameChanged = 0x0020,
+            HideWindow = 0x0080,
+            DoNotActivate = 0x0010,
+            DoNotCopyBits = 0x0100,
+            IgnoreMove = 0x0002,
+            DoNotChangeOwnerZOrder = 0x0200,
+            DoNotRedraw = 0x0008,
+            DoNotReposition = 0x0200,
+            DoNotSendChangingEvent = 0x0400,
+            IgnoreResize = 0x0001,
+            IgnoreZOrder = 0x0004,
+            ShowWindow = 0x0040,
+        }
+     
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = File.ReadAllText("1.txt", Encoding.Default);
+
+        }
+         
+        private void button3_Click(object sender, EventArgs e)
+        {
+               Process[] anotherApps = Process.GetProcessesByName("MEmu");           
+               SetForegroundWindow(anotherApps[0].MainWindowHandle);
+               SendKeys.SendWait(textBox1.Text);
+        }
+
+        public enum MouseEvent
+        {
+            MOUSEEVENTF_MOVE = 0x0001,
+            MOUSEEVENTF_LEFTDOWN = 0x02,
+            MOUSEEVENTF_LEFTUP = 0x04,
+            MOUSEEVENTF_RIGHTDOWN = 0x08,
+            MOUSEEVENTF_RIGHTUP = 0x10,
+            MOUSEEVENTF_ABSOLUTE = 0x8000,
+            MOUSEEVENTF_WHEEL = 0x0800
+        }
+
+        private void ClickXY(int x, int y)
+        {
+            Process[] anotherApps = Process.GetProcessesByName("MEmu");
+            SetForegroundWindow(anotherApps[0].MainWindowHandle);
+
+            // Установите позицию окна.
+            SetWindowPos(anotherApps[0].MainWindowHandle, IntPtr.Zero, 0, 0, 300, 500, 0);
+
+            //Координаты указаны относительно текущего положения курсора
+            mouse_event(MouseEvent.MOUSEEVENTF_MOVE, -Cursor.Position.X, -Cursor.Position.Y, 0, 0);
+            mouse_event(MouseEvent.MOUSEEVENTF_MOVE, x, y, 0, 0);
+
+            mouse_event(MouseEvent.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+            mouse_event(MouseEvent.MOUSEEVENTF_LEFTUP, x, y, 0, 0);   
+        }
+
+        private void WhellXY(int x, int y)
+        {
+            Process[] anotherApps = Process.GetProcessesByName("MEmu");
+            SetForegroundWindow(anotherApps[0].MainWindowHandle);
+
+            // Установите позицию окна.
+            SetWindowPos(anotherApps[0].MainWindowHandle, IntPtr.Zero, 0, 0, 300, 500, 0);
+
+            //Координаты указаны относительно текущего положения курсора
+            mouse_event(MouseEvent.MOUSEEVENTF_MOVE, -Cursor.Position.X, -Cursor.Position.Y, 0, 0);
+            mouse_event(MouseEvent.MOUSEEVENTF_MOVE, x, y, 0, 0);
+
+            mouse_event(MouseEvent.MOUSEEVENTF_WHEEL, x, y, -120, 0);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ClickXY(90,80);         
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ClickXY(95,185);         
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ClickXY(90, 60);         
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ClickXY(95, 185);         
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            ClickXY(5, 25);         
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            button5_Click(null, null);
+            Thread.Sleep(1000);
+
+            button11_Click(null, null);
+            Thread.Sleep(1000);
+
+            button6_Click(null, null);
+            Thread.Sleep(1000);
+            
+            button12_Click(null, null);
+            Thread.Sleep(1000);
+
+            button7_Click(null, null);
+            Thread.Sleep(1000);
+
+            button8_Click(null, null);
+            numericUpDown1.Value++;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            //for (int i = 0; i < 10; i++)
+            //    button11_Click(null, null);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < numericUpDown1.Value; i++)
+            {
+                WhellXY(5, 25);
+                Thread.Sleep(100);
+            }
+        }
+        
+        [DllImport("user32.dll")]
+        public static extern bool PostMessage(IntPtr hWnd, uint Msg, uint wParam, int lParam);
+        
+        const uint MAPVK_VK_TO_VSC = 0x00;
+        const uint MAPVK_VSC_TO_VK = 0x01;
+        const uint MAPVK_VK_TO_CHAR = 0x02;
+        const uint MAPVK_VSC_TO_VK_EX = 0x03;
+        const uint MAPVK_VK_TO_VSC_EX = 0x04;
+
+        class extraKeyInfo
+        {
+            public ushort repeatCount;
+            public char scanCode;
+            public ushort extendedKey, prevKeyState, transitionState;
+
+            public int getint()
+            {
+                return repeatCount | (scanCode << 16) | (extendedKey << 24) |
+                    (prevKeyState << 30) | (transitionState << 31);
+            }
+        };
+
+
+        [DllImport("user32.dll")]
+        static extern int MapVirtualKey(int uCode, uint uMapType);
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern short VkKeyScan(char ch);
+
+        public static void SendK(IntPtr wnd, Char ch, Int32 del)
+        {
+            uint vkCode = (byte)(VkKeyScan(ch));
+            extraKeyInfo lParam = new extraKeyInfo();
+            lParam.scanCode = (char)MapVirtualKey((int)vkCode, MAPVK_VK_TO_VSC);
+            PostMessage(wnd, WM_KEYDOWN, 'ы', lParam.getint()); //'ы'
+            lParam.repeatCount = 1;
+            lParam.prevKeyState = 1;
+            lParam.transitionState = 1;
+            System.Threading.Thread.Sleep(del);
+            //PostMessage(wnd, WM_CHAR, (IntPtr)251, new IntPtr(0));
+            PostMessage(wnd, WM_KEYUP, 'ы', lParam.getint());
+        }
+
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+
+        private static void SendCtrlhotKey(char key)
+        {
+            keybd_event(0x11, 0, 0, 0);
+            keybd_event((byte)key, 0, 0, 0);
+            keybd_event((byte)key, 0, 0x2, 0);
+            keybd_event(0x11, 0, 0x2, 0);
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Process[] anotherApps = Process.GetProcessesByName("MEmu");
+            SetForegroundWindow(anotherApps[0].MainWindowHandle);
+            string s = textBox1.Text; // (win1251.GetString(win1251Bytes));
+            Clipboard.SetText(s, TextDataFormat.Text);
+            Clipboard.SetText(s, TextDataFormat.UnicodeText);
+            SendCtrlhotKey('V');
+
+        }
+    }
+}
